c8e128a46bdf0c8f75dd5028b8bca5e9e6b6b72f
Prashant Godhwani <prashant.godhwani@hotmail.com>
2021-06-02 23:03:03 +0530

    Leetcode - 31/05/2021

--- src/June_Challenge/Leetcode_InterleavingString_02June_97.java
+++ src/June_Challenge/Leetcode_InterleavingString_02June_97.java
@@ -6,20 +6,7 @@ public class Leetcode_InterleavingString_02June_97 {
 
     public static void main(String[] args) {
         String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
-            Boolean[][][] dp = new Boolean[2][s1.length() + 1][s2.length() + 1];
-//        for(Boolean[] d : dp){
-//            System.out.println(Arrays.toString(d));
-//        }
-        boolean res = isInterleaveUtil(s1, s2, s3, 0, 0, 0, 2, dp);
-        res = res || isInterleaveUtil(s1, s2, s3, 0, 0, 0, 1, dp);
-        System.out.println(res);
-
-        boolean ress = isInterleave(s1, s2, s3, 0, 0, 0, 2, "");
-        ress = ress || isInterleave(s1, s2, s3, 0, 0, 0, 1, "");
-        System.out.println(ress);
-//        for(Boolean[] d : dp){
-//            System.out.println(Arrays.toString(d));
-//        }
+        System.out.println(isInterleave(s1, s2, s3));
     }
 
     public static boolean isInterleave(String s1, String s2, String s3, int s1p, int s2p, int s3p, int last, String asf) {
@@ -58,10 +45,25 @@ public class Leetcode_InterleavingString_02June_97 {
         return res;
     }
 
+    public static boolean isInterleave(String s1, String s2, String s3) {
+
+        if(s1.equals("")){
+            return s3.equals(s2);
+        }else if(s2.equals("")){
+            return s3.equals(s1);
+        }
+
+        if(s1.length() + s2.length() != s3.length()) return false;
+
+        Boolean[][][] dp = new Boolean[2][s1.length() + 1][s2.length() + 1];
+        boolean res = isInterleaveUtil(s1, s2, s3, 0, 0, 0, 2, dp);
+        res = res || isInterleaveUtil(s1, s2, s3, 0, 0, 0, 1, dp);
+        return res;
+    }
+
     public static boolean isInterleaveUtil(String s1, String s2, String s3, int s1p, int s2p, int s3p, int last, Boolean[][][] dp) {
         if(s3p == s3.length()){
             if(s1p == s1.length() && s2p == s2.length()) {
-                System.out.println("***************");
                 return dp[last - 1][s1p][s2p] = true;
             }
             return dp[last - 1][s1p][s2p] = false;
@@ -75,7 +77,7 @@ public class Leetcode_InterleavingString_02June_97 {
             for(int i = s1p + 1; i <= s1.length(); i++){
                 String tmp = s1.substring(s1p, i);
                 int len = tmp.length();
-                if(tmp.equals(s3.substring(s3p, s3p + len))) {
+                if(s3p + len <= s3.length() && tmp.equals(s3.substring(s3p, s3p + len))) {
                     res = isInterleaveUtil(s1, s2, s3, i, s2p, s3p + len, 1, dp);
                     if(res) return res;
                 }
@@ -85,7 +87,7 @@ public class Leetcode_InterleavingString_02June_97 {
             for(int i = s2p + 1; i <= s2.length(); i++){
                 String tmp = s2.substring(s2p, i);
                 int len = tmp.length();
-                if(tmp.equals(s3.substring(s3p, s3p + len))) {
+                if(s3p + len <= s3.length() && tmp.equals(s3.substring(s3p, s3p + len))) {
                     res = isInterleaveUtil(s1, s2, s3, s1p, i, s3p + len, 2, dp);
                     if(res) return res;
                 }
